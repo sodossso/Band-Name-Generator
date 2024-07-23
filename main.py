15264a31da01714b1821891637c8cd649a949f66
Sofia <sofianunes96@gmail.com>
2024-07-23 14:01:47 +0100

    Update main.py
    Added some details to why I wrote the code with \n

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 print("Welcome to the Band Name Generator.")
 #2. Ask the user for the city that they grew up in.
 city_name=input("What's the name of the city you grew up in?\n")
-
+#"\n" is used so that the cursor of the prompt shows in a new line of code.
 #3. Ask the user for the name of a pet.
 pet_name=input("What is your pet's name?\n")
 #4. Combine the name of their city and pet and show them their band name.
